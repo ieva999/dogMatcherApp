1568feb11556c1f565e45a236799236d99ac745b
Diomidis Anadiotis <2284736a@student.gla.ac.uk>
2017-03-09 10:24:13 +0000

    Creating models and forms

--- app/models.py
+++ app/models.py
@@ -1,5 +1,32 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    name = models.CharField(max_length=64, unique=True)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user.username
+    def __unicode__(self):
+        return self.user.username
+
+class Dog(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    #views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Dog'
+
+    def __str__ (self):
+        return self.name
+    def __unicode__(self):
+        return self.name
--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,5 @@ from app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
-    url(r'^account/(?P<user_name_slug>[\w\-]+)/$', views.account_mod, name='account_mod'),
-    url(r'^alldogs/', views.alldogs, name='alldogs'),
-    url(r'^contact/', views.contact, name='contact'),
-    url(r'^likeddogs/', views.likeddogs, name='likeddogs'),
-    url(r'^matches/', views.matches, name='matches'),
-    url(r'^survey1/', views.survey1, name='survey1'),
+    
 ]
