fc8fb120ba694863317a82f2f940ad3be5fac49b
Ieva <Ieva>
2017-03-12 15:54:35 +0000

    Connected Index and Base, updated URLs

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,8 @@ from app import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^survey/$', views.survey, name='survey'),
+    url(r'^alldogs/$', views.alldogs, name='alldogs'),
 ]
