d8109318467efdc152deefcee7f560494f03782d
Diomidis Anadiotis <2284736a@student.gla.ac.uk>
2017-03-15 14:43:45 +0000

    adding jquery

--- dogMatcher/urls.py
+++ dogMatcher/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from app import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^app/', include('app.urls')),
     url(r'^admin/', admin.site.urls),
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_DIR)
